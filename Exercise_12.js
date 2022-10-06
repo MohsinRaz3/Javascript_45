c0eb2075196f9d7c9121eabc3b61c327481ea54b
mohsinraz3 <mohsinraz3@gmail.com>
2022-10-06 20:10:43 +0500

    12/13 exe

---
+++ Exercise_12.js
@@ -0,0 +1,9 @@
+//exe 12
+let paigham = " Kiya haal hai dost? "
+let names = ["Ahsan","shahzoor","Ammar","Abrar"];
+
+console.log(` ${names[0]} ${paigham}\n ${names[1]} ${paigham}\n ${names[2]} ${paigham}\n ${names[3]} ${paigham}`)
+
+
+
+
---
+++ Exercise_13.js
@@ -0,0 +1,5 @@
+let sawari = ["Skateboard","bmx","chingchi","one-wheel"];
+
+for(i=0; i < sawari.length; i++){
+    console.log(`${sawari[i]} Meri pasandida swari hai`)
+}
