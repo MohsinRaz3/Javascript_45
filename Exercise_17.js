cef7bbf99385c96c76b59c579a37afd0c43345da
mohsinraz3 <mohsinraz3@gmail.com>
2022-10-11 00:02:18 +0500

    17th exe

---
+++ Exercise_17.js
@@ -0,0 +1,17 @@
+let invitee = ["Elon", "Haris", "bohemia", "Daniel"]
+for(i=0; i<invitee.length; i++){
+    
+    console.log(`${invitee[i]} We have found a bigger table`)
+
+}
+
+/* Add one new guest to the beginning of your array. */
+
+invitee.splice(0,-1," Shahzoor")
+
+console.log(`${invitee[i]} Added newly invited guest`)
+
+
+// 17th exe starts here
+console.log("I'm inviting only two people");
+
